eff443bf36abdf36e575db3974b50bb114ae9234
SHUBHAM VISHNU KHEDEKAR <98042453+shubhkh@users.noreply.github.com>
2024-10-20 13:12:01 +0530

    Add files via upload

---
+++ 1_Even_Odd_no.py
@@ -0,0 +1,14 @@
+def check_EvenOdd(num):
+    if num%2==0:
+        print('Even no')
+    else:
+        print('Odd no')
+
+check_EvenOdd(18)
+check_EvenOdd(13)
+
+"""num=12
+if (num%2==0):
+    print('Even')
+else:
+    print('ODD')"""
