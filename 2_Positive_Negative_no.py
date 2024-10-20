c7736f1b62b4bd981e799dab81d79e77ece42b90
SHUBHAM VISHNU KHEDEKAR <98042453+shubhkh@users.noreply.github.com>
2024-10-20 13:23:37 +0530

    Add files via upload

---
+++ 2_Positive_Negative_no.py
@@ -0,0 +1,20 @@
+def pos_neg(num):
+    if num==0:
+        print('Num is Zero')
+
+    elif(num>0):
+        print('Num is Positive')
+
+    else:
+        ('Num is Negative')
+
+pos_neg(0)
+
+
+"""num=0
+if (num==0):
+    print('Num is Zero')
+elif(num>0):
+    print('Positive num')
+else:
+    print('Negative num')"""
